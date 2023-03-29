2cbe9d95c38105b7ce08b9f565ae470bc243154f
Cedrick1101 <Cedrickkihilu73@gmail.com>
2023-03-29 16:32:42 +0200

    configure MySql Database and create employee class

---
+++ src/main/java/com/javacode/employeemanagementapp/model/Employee.java
@@ -0,0 +1,27 @@
+package com.javacode.employeemanagementapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "Employee_tbl")
+@Entity
+public class Employee {
+    @Getter
+    @Setter
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_address")
+    private String email;
+
+}
